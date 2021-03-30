f5c4906de5a8b3870718a193f1901890acb884f7
clanting1997 <54062662+clanting1997@users.noreply.github.com>
2021-03-30 14:38:10 +0200

    project setup pt2

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+
+    devServer: {
+        contentBase: "./dist",
+    },
+    
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        })
+    ]
+}
