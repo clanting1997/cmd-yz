4922c5deda29991a260b17b10ccda4e706f39159
clanting1997 <54062662+clanting1997@users.noreply.github.com>
2021-03-30 15:57:41 +0200

    project setup pt3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('hello world');
+
+const recorderOne = document.getElementById('recorder1');
+const playerOne = document.getElementById('player1');
+
+console.log(recorderOne, playerOne);
+
+recorderOne.addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    const url = URL.createObjectURL(file);
+    // Do something with the audio file.
+    playerOne.src = url;
+});
